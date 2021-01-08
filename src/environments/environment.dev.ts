c29a1df16cc754e48ac92395d7ec5c74a17adfa5
Ritambuj <ritambuj@gmail.com>
2021-01-08 23:55:10 +0530

    master branch first commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  serverUrl: 'https://console.sbaonline.in/api',
+  appUrl: 'https://sbaonline.in',
+  appImageUrl: 'https://www.tryion.shop/assets/imgs/ionshop.png',
+  appId: 'Nmw2P51lZC',
+  fbId: '579343976048705',
+  stripePublicKey: 'YOUR_STRIPE_PUBLIC_KEY',
+  androidHeaderColor: '#222428',
+  defaultLang: 'en',
+  googleClientId: '145788320772-dql4tumol715mc6cnjgshlidj8q3t8lq.apps.googleusercontent.com',
+  currency: {
+    code: 'USD',
+    display: 'symbol',
+    digitsInfo: '1.2-2',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,30 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  serverUrl: 'https://console.sbaonline.in/api',
+  appUrl: 'https://sbaonline.in',
+  appImageUrl: 'https://www.tryion.shop/assets/imgs/ionshop.png',
+  appId: 'Nmw2P51lZC',
+  fbId: '579343976048705',
+  stripePublicKey: 'YOUR_STRIPE_PUBLIC_KEY',
+  androidHeaderColor: '#222428',
+  defaultLang: 'en',
+  googleClientId: '145788320772-dql4tumol715mc6cnjgshlidj8q3t8lq.apps.googleusercontent.com',
+  currency: {
+    code: 'INR',
+    display: 'symbol',
+    digitsInfo: '1.2-2',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
